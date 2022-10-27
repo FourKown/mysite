513053a78cdfedbddd80eccb1e8896bb5600fcb9
FourKown <dydqndaos123@naver.com>
2022-10-27 12:52:58 +0900

    데이터 읽기(익산시 추가)

--- testmap/views.py
+++ testmap/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+import csv 
 
+IKSAN = []
+with open('mysite\static\IKSAN_Data.csv') as csvfile:
+    lines = csv.reader(csvfile, delimiter=',')
+    for i in lines:
+        IKSAN.append(i)
+
+data = IKSAN[1:11]
 
 def index(request):
-    return render(request, 'testmap/index.html')
-    
+    return render(request, 'testmap/index.html', {'Data':data})
